a86eadc1218cbf3ef7fbd15d1ed98db2ec655588
ianghel <angheliuliaioana@gmail.com>
2021-12-21 01:36:22 +0200

    Create model for activity

---
+++ model.py
@@ -0,0 +1,8 @@
+class Activity:
+    def __init__(self, title, day, organizer, price, location, category):
+        self.title = title
+        self.day = day
+        self.organizer = organizer
+        self.price = price
+        self.location = location
+        self.category = category
