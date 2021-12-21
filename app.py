fd72c96791e3965b5fbe5620543864d558351d7b
ianghel <angheliuliaioana@gmail.com>
2021-12-22 01:23:17 +0200

    Map Filtering Cities

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ import numpy as np
 import pydeck as pdk
 
 
-def show_map():
+df_activities = pd.DataFrame([a.__dict__ for a in activities])
+
+
+def show_map(custom=None):
     locations = {
         "Bucharest": {
             "lat": 44.439663,
@@ -34,21 +37,30 @@ def show_map():
         longitude=25,
         zoom=5.5
     )
-    layer = pdk.Layer(
-        type='HeatmapLayer',
-        data=df_activities,
-        pickable=True,
-        opacity=0.9,
-        get_position=["lon", "lat"],
-        get_radius=200,
-    )
+    if custom:
+        df = df_activities.loc[df_activities['location'] == custom]
+        layer = pdk.Layer(
+            type='HeatmapLayer',
+            data=df,
+            pickable=True,
+            opacity=0.9,
+            get_position=["lon", "lat"],
+            get_radius=200,
+        )
+    else:
+        layer = pdk.Layer(
+            type='HeatmapLayer',
+            data=df_activities,
+            pickable=True,
+            opacity=0.9,
+            get_position=["lon", "lat"],
+            get_radius=200,
+        )
     r = pdk.Deck(layers=[layer], initial_view_state=view_ro,
                  height=800, tooltip=True)
 
     st.pydeck_chart(r)
 
-
-df_activities = pd.DataFrame([a.__dict__ for a in activities])
 
 st.set_page_config(
     page_title="XPARSER",
@@ -58,7 +70,7 @@ st.set_page_config(
 )
 st.title("🎄Activities for Christmas🎄🎁")
 show = pd.DataFrame({
-    'options1': ['All Activities', 'Activities on a day', 'Activities based on category', 'Activities based on location', 'Activities by price', 'Custom search']
+    'options1': ['All Activities', 'Activities on a day', 'Activities based on category', 'Activities based on location', 'Activities by price', 'Custom Search']
 })
 
 option = st.sidebar.selectbox('Show:', show['options1'])
@@ -80,7 +92,7 @@ elif option == "Activities on a day":
         st.subheader('No activities for this day!')
 elif option == "Activities based on category":
     categories = pd.DataFrame({
-        'options': ['family', 'children', 'teens', 'All']})
+        'options': ['Family', 'Children', 'Teens', 'All']})
     category_selected = st.sidebar.selectbox(
         'Select category:', categories['options'])
     if category_selected == 'All':
@@ -91,7 +103,7 @@ elif option == "Activities based on category":
     else:
         by_category = []
         for activity in activities:
-            if activity.category == category_selected:
+            if activity.category == category_selected.lower():
                 by_category.append(activity)
         if by_category:
             st.table(pd.DataFrame([c.__dict__ for c in by_category]))
@@ -110,18 +122,55 @@ elif option == "Activities by price":
     else:
         st.subheader('No activities for this price range!')
 elif option == "Activities based on location":
-    categories = pd.DataFrame({
+    locations = pd.DataFrame({
         'options': ['Bucharest', 'Brasov', 'Cluj', 'All']})
-    category_selected = st.sidebar.selectbox(
-        'Select city:', categories['options'])
-    if category_selected == 'All':
+    location_selected = st.sidebar.selectbox(
+        'Select city:', locations['options'])
+    if location_selected == 'All':
         show_map()
     else:
         by_location = []
         for activity in activities:
-            if activity.location == category_selected:
+            if activity.location == location_selected:
                 by_location.append(activity)
         if by_location:
             st.table(pd.DataFrame([c.__dict__ for c in by_location]))
         else:
             st.subheader('No activities for this location!')
+elif option == 'Custom Search':
+    agree = st.sidebar.checkbox('Show Map')
+    price_range = st.sidebar.slider("Choose a price range:", value=[0, 100])
+    categories = pd.DataFrame({
+        'options': ['Family', 'Children', 'Teens', 'All']})
+    category_selected = st.sidebar.selectbox(
+        'Select category:', categories['options'])
+    date_selected = st.sidebar.date_input(
+        'Day are you searching for:')
+    locations = pd.DataFrame({
+        'options': ['Bucharest', 'Brasov', 'Cluj', 'All']})
+    location_selected = st.sidebar.selectbox(
+        'Select city:', locations['options'])
+    by_price = set()
+    by_location = set()
+    by_category = set()
+    by_date = set()
+    for activity in activities:
+        if price_range[0] < int(activity.price) < price_range[1]:
+            by_price.add(activity)
+        if activity.location == location_selected or location_selected == 'All':
+            by_location.add(activity)
+        if activity.category == category_selected.lower() or category_selected == 'All':
+            by_category.add(activity)
+        if activity.day == str(date_selected):
+            by_date.add(activity)
+    custom_search = set.intersection(
+        by_price, by_location, by_category, by_date)
+    if agree:
+        if location_selected != 'All':
+            show_map(location_selected)
+        else:
+            show_map()
+    if custom_search:
+        st.table(pd.DataFrame([c.__dict__ for c in custom_search]))
+    else:
+        st.subheader('No activities found!')
